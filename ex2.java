233e6514a401d28682e54d8ca23e71d86e734c42
arpao007 <pao123zpao123z@gmail.com>
2025-11-22 22:46:28 +0700

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -1,18 +1,28 @@
 package Op;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ex2 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		try {
+			
 		System.out.print("Enter you number1 : ");
-		double number1 = sc.nextDouble();
+		double numberOne = sc.nextDouble();
 		
 		System.out.print("Enter you number2 : ");
-		double number2 = sc.nextDouble();
+		double numberTwo = sc.nextDouble();
 		
-		double Rectangle = number1 * number2;
-		System.out.print("is you number1 :"+ number1 + "\n" +"it you number2 :" +number2 + "\n" + "Value of a rectangle :" + Rectangle );
-		}
-
+		double RectangleArea = numberOne * numberTwo;
+		
+		System.out.println("Your number1: " + numberOne);
+        System.out.println("Your number2: " + numberTwo);
+        System.out.println("Value of rectangle area: " + RectangleArea);
+        
+		}catch (InputMismatchException e) {
+            System.out.println("Error: Please enter a valid number.");
+        }
+		sc.close();
+	}
 }

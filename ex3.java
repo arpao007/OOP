e8b870ecf240b19e6e9072120a3c9e543916ea1b
arpao007 <pao123zpao123z@gmail.com>
2025-11-22 22:58:02 +0700

    Update ex3.java

--- ex3.java
+++ ex3.java
@@ -1,20 +1,25 @@
 package Op;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class ex3 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Enter you number :");
-		int number = sc.nextInt();
-		
-		if (number % 2 == 0) {
-			System.out.print( "Even");
-			}
-			else {
-				System.out.print("Odd");
-		}
-		sc.close();
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.print("Enter your number: ");
+            int number = sc.nextInt();
+
+            if (number % 2 == 0) {
+                System.out.println("Even");
+            } else {
+                System.out.println("Odd");
+            }
+
+        } catch (InputMismatchException e) {
+            System.out.println("Error: Please enter a valid number.");
+        }
+
+        sc.close();
+    }
+}

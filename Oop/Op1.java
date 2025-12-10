ca97abefe80e3c145fe432c6363804d11a2c882d
Natthaphon Saewang <pao123zpao123z@gmail.com>
2025-12-10 17:24:05 +0700

    oop3.1

---
+++ Oop/Op1.java
@@ -0,0 +1,12 @@
+package Oop;
+
+public class Op1 {
+	private String username;
+	
+	public Op1(String username) {
+		this.username = username;
+	}
+	public String getUsername() {
+		return username;
+	}
+}

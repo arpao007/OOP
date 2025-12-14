d7c52f329906ab46ea72dd37c42fe9400b71661d
arpao007 <pao123zpao123z@gmail.com>
2025-12-14 23:19:19 +0700

    Update M4.java

--- 3.4/M4.java
+++ 3.4/M4.java
@@ -3,20 +3,31 @@ package Oop;
 import java.util.Scanner;
 
 public class M4 {
+
     public static void main(String[] args) {
-
         try (Scanner sc = new Scanner(System.in)) {
-        	System.out.print("Number of products : ");
-            int n = sc.nextInt();
-            sc.nextLine();
-
-            for (int i = 0; i < n; i++) {
-            	System.out.print("Product name " + (i+1) + " : " );
-                String productName = sc.nextLine();
-                new Op4(productName); 
-            }
-
-            System.out.println("Includes all product lists : " + Op4.getProductCount());
+            int n = readProductCount(sc);
+            readProducts(sc, n);
+            printResult();
         }
+    }
+
+    private static int readProductCount(Scanner sc) {
+        System.out.print("Number of products : ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        return n;
+    }
+
+    private static void readProducts(Scanner sc, int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.print("Product name " + (i + 1) + " : ");
+            String productName = sc.nextLine();
+            new Op4(productName);
+        }
+    }
+
+    private static void printResult() {
+        System.out.println("Includes all product lists : " + Op4.getProductCount());
     }
 }
